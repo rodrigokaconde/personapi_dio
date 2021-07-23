a6ee9e01cafca299cbdaf8166bc28167e4393780
rodrigokaconde <rodrigokaconde@yahoo.com.br>
2021-07-23 10:32:20 -0300

    criação do metodo para listar todas as pessoas

--- src/main/java/br/com/yahoo/rodrigokaconde/personapi/service/PersonService.java
+++ src/main/java/br/com/yahoo/rodrigokaconde/personapi/service/PersonService.java
@@ -8,6 +8,9 @@ import br.com.yahoo.rodrigokaconde.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class PersonService {
@@ -26,5 +29,12 @@ public class PersonService {
         return MessageReponseDTO
                 .builder()
                 .message("Created person with ID"+savedPerson.getId()).build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
